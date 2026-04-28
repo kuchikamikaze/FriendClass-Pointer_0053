13f7a8f86770ac41a3f9494d307824811901f561
Naufal Arif <na713634@gmail.com>
2026-04-28 15:24:51 +0700

    deklarasi class siswa

--- friendClass.cpp
+++ friendClass.cpp
@@ -1,3 +1,12 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class siswa;
+class orang {
+    private:
+        string nama;
+    public:
+    void setNama(string pNama);
+    
+};
